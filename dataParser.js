ba4da7a404e79fd2e05afd617917f5951fb80160
rs222kn <rs222kn@student.lnu.se>
2016-09-25 19:38:18 +0200

    changed tab style, now returns null if empty result, removed unnecessary booking object

--- dataParser.js
+++ dataParser.js
@@ -8,25 +8,25 @@ const _getTodaysDate = () => new Date();
 const _parseDate =  dateString => new Date(dateString);
 
 const getSearchData = html => {
-    let $ = _loadHtml(html);
-    let dataIds = $('.searchObject').map((i, elem) => {
-        return {
-            id: $(elem).data('id'),
-            name: $(elem).data('name')
-        };
-    }).get();
-    return dataIds.length > 0 ? dataIds : dataIds[0];
+  let $ = _loadHtml(html);
+  let dataIds = $('.searchObject').map((i, elem) => {
+    return {
+      id: $(elem).data('id'),
+      name: $(elem).data('name')
+    };
+  }).get();
+  return dataIds.length > 0 ? dataIds : dataIds[0];
 };
 
 const getTypes = html => {
-    let $ = _loadHtml(html);
-    let types = $('#fancytypeselector option').map((i, elem) => {
-        return {
-            name: $(elem).text(),
-            value: $(elem).val()
-        };
-    }).get();
-    return types;
+  let $ = _loadHtml(html);
+  let types = $('#fancytypeselector option').map((i, elem) => {
+    return {
+      name: $(elem).text(),
+      value: $(elem).val()
+    };
+  }).get();
+  return types;
 };
 
 /**
@@ -35,8 +35,8 @@ const getTypes = html => {
  * @return {[string]}      [searched item name]
  */
 const getSearchId = html => {
-    let $ = _loadHtml(html);
-    return $('#searchTextWide').text().trim();
+  let $ = _loadHtml(html);
+  return $('#searchTextWide').text().trim();
 };
 
 /**
@@ -46,24 +46,22 @@ const getSearchId = html => {
  * @return {[object]}        [sorted schedule infromation]
  */
 const buildSchedule = (object, id) => {
-    id = id || null;
-    if(!object.hasOwnProperty('reservations')){ throw 'Invalid search result'; }
-    return object.reservations
-        .map((reservation) => {
-            return {
-                booking: {
-                    time:{
-                        startDate: reservation.startdate,
-                        startTime: reservation.starttime,
-                        endDate: reservation.enddate,
-                        endTime: reservation.endtime
-                    },
-                    id,
-                    bookingId: reservation.id,
-                    columns: reservation.columns
-                }
-            };
-        });
+  id = id || null;
+  if(!object.hasOwnProperty('reservations')){ throw 'Invalid search result'; }
+  return object.reservations
+    .map(reservation => {
+      return {
+        time:{
+          startDate: reservation.startdate,
+          startTime: reservation.starttime,
+          endDate: reservation.enddate,
+          endTime: reservation.endtime
+        },
+        searchId: id,
+        bookingId: reservation.id,
+        columns: reservation.columns
+      };
+  });
 };
 
 /**
@@ -72,28 +70,28 @@ const buildSchedule = (object, id) => {
  * @return {[object]}        [only todays room schedule information]
  */
 const buildTodaysSchedule = (object, id) => {
-    let todaysSchedule = buildSchedule(object, id)
-        .filter((reservation) => {
-            let reservationDate = _parseDate(reservation.booking.time.startDate);
-            let todaysDate = _getTodaysDate();
+  let todaysSchedule = buildSchedule(object, id)
+    .filter((reservation) => {
+      let reservationDate = _parseDate(reservation.booking.time.startDate);
+      let todaysDate = _getTodaysDate();
 
-            return reservationDate.getFullYear() === todaysDate.getFullYear() &&
-                reservationDate.getMonth() === todaysDate.getMonth() &&
-                reservationDate.getDate() === todaysDate.getDate();
-        });
-    return todaysSchedule.length > 0 ? todaysSchedule : [{ id }] ;
+      return reservationDate.getFullYear() === todaysDate.getFullYear() &&
+        reservationDate.getMonth() === todaysDate.getMonth() &&
+        reservationDate.getDate() === todaysDate.getDate();
+    });
+  return todaysSchedule.length > 0 ? todaysSchedule : null;
 };
 
 const isValidSearch =  html => {
-    let $ = _loadHtml(html);
-    return $('.searchObject').hasOwnProperty('0');
+  let $ = _loadHtml(html);
+  return $('.searchObject').hasOwnProperty('0');
 };
 
 module.exports = {
-    isValidSearch,
-    buildTodaysSchedule,
-    buildSchedule,
-    getSearchId,
-    getTypes,
-    getSearchData
+  isValidSearch,
+  buildTodaysSchedule,
+  buildSchedule,
+  getSearchId,
+  getTypes,
+  getSearchData
 };
